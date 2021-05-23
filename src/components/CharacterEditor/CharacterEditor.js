91d5236c5d679b070904de59daaa9afd198640c4
mornir <jeromepott@hotmail.com>
2021-05-23 10:03:30 +0200

    Last round of corrects

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -25,6 +25,7 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.perspectiveDecoration}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
@@ -73,7 +74,6 @@ function App() {
         </div>
       </MaxWidthWrapper>
 
-      <div className={styles.perspectiveDecoration}></div>
       <div className={styles.characterWrapper}>
         <Character
           body={body}
